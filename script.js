893db9735aa05fc642d43b5f38f8c0caade19926
Daniela Carballo <danscarballo@gmail.com>
2023-02-25 04:10:44 -0300

    Cambio de imagenes y funcion duplicarImagen

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ const imagen9 = document.querySelector('#imagen9');
 const imagen10 = document.querySelector('#imagen10');
 
 const IMAGENES = [imagen1, imagen2, imagen3, imagen4, imagen5, imagen6, imagen7, imagen8, imagen9, imagen10];
+
+const duplicarImagenes = function(){
+    IMAGENES.map((imagen, index) => {
+        const imagenDuplicada = imagen.cloneNode(true);
+        imagenDuplicada.id = `imagen${index + 11}`;
+        document.querySelector('#imagenes').appendChild(imagenDuplicada);
+    })
+}
+
+duplicarImagenes();
