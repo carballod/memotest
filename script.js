3beac8e0048cc29afc63b16fc2a59bf173be09d9
Daniela Carballo <danscarballo@gmail.com>
2023-02-25 00:31:30 -0300

    Añade imagenes y sus estilos

---
+++ script.js
@@ -0,0 +1,12 @@
+const imagen1 = document.querySelector('#imagen1');
+const imagen2 = document.querySelector('#imagen2');
+const imagen3 = document.querySelector('#imagen3');
+const imagen4 = document.querySelector('#imagen4');
+const imagen5 = document.querySelector('#imagen5');
+const imagen6 = document.querySelector('#imagen6');
+const imagen7 = document.querySelector('#imagen7');
+const imagen8 = document.querySelector('#imagen8');
+const imagen9 = document.querySelector('#imagen9');
+const imagen10 = document.querySelector('#imagen10');
+
+const IMAGENES = [imagen1, imagen2, imagen3, imagen4, imagen5, imagen6, imagen7, imagen8, imagen9, imagen10];
