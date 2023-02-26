d7d87aece8fa48c09a1bd0d867ef0479150395b2
Daniela Carballo <danscarballo@gmail.com>
2023-02-26 14:10:22 -0300

    Arregla el error que no mezclaba todas las imagenes

--- script.js
+++ script.js
@@ -11,23 +11,24 @@ const imagen10 = document.querySelector('#imagen10');
 
 const IMAGENES = [imagen1, imagen2, imagen3, imagen4, imagen5, imagen6, imagen7, imagen8, imagen9, imagen10];
 
+const mezclarImagenes = function(){
+    return IMAGENES
+        .flatMap((imagen) => [imagen, imagen])
+        .sort(() => Math.random() - 0.5)
+        .forEach((imagen, index) => {
+            imagen.style.order = index;
+        });
+}; 
+
+
 const duplicarImagenes = function(){
     IMAGENES.map((imagen, index) => {
         const imagenDuplicada = imagen.cloneNode(true);
         imagenDuplicada.id = `imagen${index + 11}`;
         document.querySelector('#imagenes').appendChild(imagenDuplicada);
+        IMAGENES.push(imagenDuplicada); // Agregar la imagen duplicada al arreglo
     })
 }
-
-const mezclarImagenes = function(){
-    IMAGENES
-    .flatMap((imagen) => [imagen, imagen])
-    .sort(() => Math.random() - 0.5)
-    .forEach((imagen, index) => {
-        imagen.style.order = index;
-    }
-    )
-};
 
 
 const iniciar = function(){
