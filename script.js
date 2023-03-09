4b1e7e493b9bb6bca2f653a986fee40c38fd83f6
carballod <109915531+carballod@users.noreply.github.com>
2023-03-09 12:05:15 -0300

    Añade simplicidad al codigo

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-const imagen1 = document.querySelector('#imagen1');
-const imagen2 = document.querySelector('#imagen2');
-const imagen3 = document.querySelector('#imagen3');
-const imagen4 = document.querySelector('#imagen4');
-const imagen5 = document.querySelector('#imagen5');
-const imagen6 = document.querySelector('#imagen6');
-const imagen7 = document.querySelector('#imagen7');
-const imagen8 = document.querySelector('#imagen8');
-const imagen9 = document.querySelector('#imagen9');
-const imagen10 = document.querySelector('#imagen10');
+const IMAGENES = Array.from(document.getElementsByTagName("img"));
 
-const IMAGENES = [imagen1, imagen2, imagen3, imagen4, imagen5, imagen6, imagen7, imagen8, imagen9, imagen10]
-
-const mezclarImagenes = function(){
+const mezclarAleatoriamenteImagenesDuplicadas = function(){
     const imagenesMezcladas = IMAGENES
         .flatMap((imagen) => [imagen, imagen])
         .sort(() => Math.random() - 0.5);
         
-    imagenesMezcladas.forEach((imagen, index) => {
-        imagen.style.order = index;
-    });
+    establecerOrdenDeImagenes(imagenesMezcladas);
 
     return imagenesMezcladas;
 };
+
+const establecerOrdenDeImagenes = function(imagenes){
+    imagenes.forEach((imagen, index) => {
+    imagen.style.order = index;
+    });
+}
 
 const duplicarImagenes = function(){
     IMAGENES.forEach((imagen, index) => {
@@ -71,7 +64,7 @@ const ronda = function(event){
     const imagenSeleccionada = event.target;
     if (imagenSeleccionada.classList.contains('seleccionada') || seleccionImagen1 && seleccionImagen2) {
         return;
-      }
+    }
 
     imagenSeleccionada.classList.remove('ocultar');
     imagenSeleccionada.classList.add('seleccionada');
@@ -108,7 +101,7 @@ const ronda = function(event){
 
 const iniciarJuego = function(){
     duplicarImagenes();
-    mezclarImagenes();
+    mezclarAleatoriamenteImagenesDuplicadas();
     ocultarImagenes();
     
     IMAGENES.forEach((imagen) => {
